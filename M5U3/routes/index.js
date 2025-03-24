e16588016ec6dd889e83371096a7f6b49fbd7085
Emiliano Ramos <emiramos.utn@gmail.com>
2025-03-24 12:41:54 -0300

    archivos M5U3

---
+++ M5U3/routes/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  let novedades = await novedadesModel.getNovedades();
+  novedades = novedades.slice(0, 5); 
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimage.jpg'
+      }
+    }
+  });
+  
+  res.render('index', { novedades });
+});
+
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  console.log(req.body);
+
+  var obj = {
+    to: 'vertex.grafica@gmail.com',
+    subject: 'Contacto Web',
+    html: `
+      <p>${nombre} ${apellido} se contactó por medio de la web y quiere información a este correo: ${email}</p>
+      <p>Además, hizo el siguiente comentario:</p>
+      <p>${mensaje}</p>
+      <p>Su teléfono es: ${tel}</p>
+    `
+  };
+
+  try {
+    var transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+    var info = await transporter.sendMail(obj);
+
+    res.render('index', {
+      message: 'Mensaje enviado correctamente',
+    });
+  } catch (error) {
+    console.error('Error enviando el correo:', error);
+    res.render('index', {
+      message: 'Ocurrió un error al enviar el mensaje.',
+    });
+  }
+});
+
+module.exports = router;
